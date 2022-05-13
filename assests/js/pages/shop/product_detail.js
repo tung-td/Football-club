3432bc05c562f282e87534b40c6b15754bc2c130
tung-td <tranduytung33@gmail.com>
2022-05-13 18:43:04 +0700

    test js

--- assests/js/pages/shop/product_detail.js
+++ assests/js/pages/shop/product_detail.js
@@ -2,15 +2,15 @@ let thumbnails = document.getElementsByClassName('thumbnail')
 
 let activeImages = document.getElementsByClassName('active')
 
-for (var i=0; i < thumbnails.length; i++){
+for (var i = 0; i < thumbnails.length; i++) {
 
-	thumbnails[i].addEventListener('click', function(){
+	thumbnails[i].addEventListener('click', function () {
 		console.log(activeImages)
-		
-		if (activeImages.length > 0){
+
+		if (activeImages.length > 0) {
 			activeImages[0].classList.remove('active')
 		}
-		
+
 
 		this.classList.add('active')
 		document.getElementById('featured').src = this.src
@@ -21,10 +21,10 @@ for (var i=0; i < thumbnails.length; i++){
 let buttonRight = document.getElementById('slideRight');
 let buttonLeft = document.getElementById('slideLeft');
 
-buttonLeft.addEventListener('click', function(){
+buttonLeft.addEventListener('click', function () {
 	document.getElementById('slider').scrollLeft -= 180
 })
 
-buttonRight.addEventListener('click', function(){
+buttonRight.addEventListener('click', function () {
 	document.getElementById('slider').scrollLeft += 180
 })
