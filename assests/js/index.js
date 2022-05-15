6b0a6186908800c22f3326abddeb927d36242f04
Nam Phung <namphungdn134@gmail.co>
2022-05-15 09:00:44 +0700

    merge from namphung2 branch

--- assests/js/index.js
+++ assests/js/index.js
@@ -2,14 +2,33 @@ $(function() {
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 3,
+        slidesToScroll: 4,
         arrows: false,
         autoplay: false,
         autoplaySpeed: 3000,
         dots: false,
         centerMode: true,
         centerPadding: '0',
-        focusOnSelect: true
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '0',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
     $('.single-item').slick({
         autoplay: true,
@@ -74,3 +93,71 @@ $(function() {
         nextArrow: '.matches-next',
     });
 });
+
+$(function() {
+    $('.multiple-items-moments').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        centerMode: true,
+        centerPadding: '0',
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 1100,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    centerPadding: '0',
+                    slidesToShow: 3
+
+                }
+            }, {
+                breakpoint: 1023,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    centerPadding: '0',
+                    slidesToShow: 2
+
+                }
+            },
+            {
+                breakpoint: 739,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    centerPadding: '0',
+                    slidesToShow: 1
+                }
+            }
+        ]
+
+    });
+
+});
+
+$(document).ready(function() {
+    $('#loginModal').modal('');
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+});
+
+
+function openSearch() {
+    document.getElementById("search-display").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("search-display").style.display = "none";
+}
