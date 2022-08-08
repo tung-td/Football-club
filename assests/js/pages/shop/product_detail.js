ba3e5bab14e1d12efab29556867459f17f3837be
tung-td <tranduytung33@gmail.com>
2022-08-08 15:35:31 +0700

    make more color for website
    index, VNsquad(...), product detail

--- assests/js/pages/shop/product_detail.js
+++ assests/js/pages/shop/product_detail.js
@@ -17,6 +17,8 @@ for (var i = 0; i < thumbnails.length; i++) {
 	})
 }
 
+// Recently & Also like
+
 document.getElementById("show_re").onclick = function () {
 	document.getElementById("recently_button").style.display = 'flex';
 	document.getElementById("show_re").style.display = 'none';
@@ -38,5 +40,41 @@ document.getElementById("hide_al").onclick = function () {
 	document.getElementById("show_al").style.display = 'inline-block';
 	document.getElementById("hide_al").style.display = 'none';
 }
+
+// Information product (description, shipping, ...)
+
+document.getElementById("show_sh").onclick = function () {
+	document.getElementById("shipping_button").style.display = 'flex';
+	document.getElementById("show_sh").style.display = 'none';
+	document.getElementById("hide_sh").style.display = 'inline-block';
+}
+document.getElementById("hide_sh").onclick = function () {
+	document.getElementById("shipping_button").style.display = 'none';
+	document.getElementById("show_sh").style.display = 'inline-block';
+	document.getElementById("hide_sh").style.display = 'none';
+}
+
+document.getElementById("show_dt").onclick = function () {
+	document.getElementById("detail_product").style.display = 'flex';
+	document.getElementById("show_dt").style.display = 'none';
+	document.getElementById("hide_dt").style.display = 'inline-block';
+}
+document.getElementById("hide_dt").onclick = function () {
+	document.getElementById("detail_product").style.display = 'none';
+	document.getElementById("show_dt").style.display = 'inline-block';
+	document.getElementById("hide_dt").style.display = 'none';
+}
+
+document.getElementById("show_de").onclick = function () {
+	document.getElementById("description_product").style.display = 'flex';
+	document.getElementById("show_de").style.display = 'none';
+	document.getElementById("hide_de").style.display = 'inline-block';
+}
+document.getElementById("hide_de").onclick = function () {
+	document.getElementById("description_product").style.display = 'none';
+	document.getElementById("show_de").style.display = 'inline-block';
+	document.getElementById("hide_de").style.display = 'none';
+}
+
 
 
